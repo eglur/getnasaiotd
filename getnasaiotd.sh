ae40534069e770480b2ac33d1e4fd94f0a7cff6b
eglur <eglur@example.com>
2021-03-29 18:01:03 -0300

    fix: don’t ‘wget --continue’ the RSS file; yes ‘ln --force’
    ‘wget’ got confused with this flag and didn’t download the new RSS file from the website.
    
    ‘ln’ needs that flag so it can replacements the old link.

--- getnasaiotd.sh
+++ getnasaiotd.sh
@@ -5,7 +5,7 @@ mkdir -p $IOTD_DIR
 
 RSS_URL="https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss"
 RSS_FILE=$IOTD_DIR/rss.xml
-wget --continue --output-document $RSS_FILE $RSS_URL
+wget --output-document $RSS_FILE $RSS_URL
 
 XPATH="string(/rss/channel/item[1]/enclosure/@url)"
 IMG_URL=$(xmllint --xpath $XPATH $RSS_FILE)
@@ -14,4 +14,5 @@ IMG_FILE=$IOTD_DIR/$(basename $IMG_URL)
 wget --continue --output-document $IMG_FILE $IMG_URL
 
 # Create a symbolic to easily access the last IOTD
-ln -s $IMG_FILE $IOTD_DIR/iotd
+# The ‘--force’ flag is to replace the old one.
+ln --symbolic --force $IMG_FILE $IOTD_DIR/iotd
